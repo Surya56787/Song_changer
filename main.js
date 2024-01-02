876d817ebdf521c0193ba652ab9133cce386935f
Surya56787 <105086420+Surya56787@users.noreply.github.com>
2024-01-02 11:34:44 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song=""
+song2=""
+function preload(){
+    song=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function setup(){
+canvas=createCanvas(600,500);
+canvas.position(650,250);
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+image(video, 0, 0, 600, 500);
+}
